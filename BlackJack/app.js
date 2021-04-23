1bd74ce37251441d9b03bd0dd918be2fbc957be5
Brittani Savage <brittanisavage@gmail.com>
2021-04-23 13:55:25 -0500

    Added modal

--- BlackJack/app.js
+++ BlackJack/app.js
@@ -1,5 +1,24 @@
 $(() => {
-console.log("you got it dude!")
+const modal = document.getElementById("myModal");
+
+
+const btn = document.getElementById("myBtn");
+
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 let $deck = $('<img class="deckCard" id="deckcard">');
 $deck.attr('src', "https://i.ibb.co/VMD00H1/card-back-black.png");
