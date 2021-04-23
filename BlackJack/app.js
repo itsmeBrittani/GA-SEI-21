9edb76afce85c2bf835a856c834a025f998387a8
Brittani Savage <brittanisavage@gmail.com>
2021-04-23 14:23:26 -0500

    Trying to fix modal

--- BlackJack/app.js
+++ BlackJack/app.js
@@ -1,6 +1,5 @@
 $(() => {
 
-
 let $deck = $('<img class="deckCard" id="deckcard">');
 $deck.attr('src', "https://i.ibb.co/VMD00H1/card-back-black.png");
 
@@ -210,3 +209,28 @@ $standBtn.on('click', () => {
 
 });
 
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
